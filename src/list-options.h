66e1b28ab66d7acee314b18a6a9b1106ebf59798
Hironao OTSUBO <motemen@gmail.com>
2023-01-24 09:49:48 +0900

    i18n list-options.h

--- src/list-options.h
+++ src/list-options.h
@@ -11,92 +11,92 @@
 type     normal */
 OP(none,                  "",
 SPECIAL, false)
-OP(rogue_like_commands,   "Use the roguelike command keyset",
+OP(rogue_like_commands,   N_("Use the roguelike command keyset"),
 INTERFACE, false)
-OP(use_sound,             "Use sound",
+OP(use_sound,             N_("Use sound"),
 INTERFACE, false)
-OP(show_damage,           "Show damage player deals to monsters",
+OP(show_damage,           N_("Show damage player deals to monsters"),
 INTERFACE, false)
-OP(use_old_target,        "Use old target by default",
+OP(use_old_target,        N_("Use old target by default"),
 INTERFACE, false)
-OP(pickup_always,         "Always pickup items",
+OP(pickup_always,         N_("Always pickup items"),
 INTERFACE, false)
-OP(pickup_inven,          "Always pickup items matching inventory",
+OP(pickup_inven,          N_("Always pickup items matching inventory"),
 INTERFACE, true)
-OP(show_flavors,          "Show flavors in object descriptions",
+OP(show_flavors,          N_("Show flavors in object descriptions"),
 INTERFACE, false)
-OP(show_target,           "Highlight target with cursor",
+OP(show_target,           N_("Highlight target with cursor"),
 INTERFACE, true)
-OP(highlight_player,      "Highlight player with cursor between turns",
+OP(highlight_player,      N_("Highlight player with cursor between turns"),
 INTERFACE, false)
-OP(disturb_near,          "Disturb whenever viewable monster moves",
+OP(disturb_near,          N_("Disturb whenever viewable monster moves"),
 INTERFACE, true)
-OP(solid_walls,           "Show walls as solid blocks",
+OP(solid_walls,           N_("Show walls as solid blocks"),
 INTERFACE, false)
-OP(hybrid_walls,          "Show walls with shaded background",
+OP(hybrid_walls,          N_("Show walls with shaded background"),
 INTERFACE, false)
-OP(view_yellow_light,     "Color: Illuminate torchlight in yellow",
+OP(view_yellow_light,     N_("Color: Illuminate torchlight in yellow"),
 INTERFACE, false)
-OP(animate_flicker,       "Color: Shimmer multi-colored things",
+OP(animate_flicker,       N_("Color: Shimmer multi-colored things"),
 INTERFACE, false)
-OP(center_player,         "Center map continuously",
+OP(center_player,         N_("Center map continuously"),
 INTERFACE, false)
-OP(purple_uniques,        "Color: Show unique monsters in purple",
+OP(purple_uniques,        N_("Color: Show unique monsters in purple"),
 INTERFACE, false)
-OP(auto_more,             "Automatically clear '-more-' prompts",
+OP(auto_more,             N_("Automatically clear '-more-' prompts"),
 INTERFACE, false)
-OP(hp_changes_color,      "Color: Player color indicates % hit points",
+OP(hp_changes_color,      N_("Color: Player color indicates % hit points"),
 INTERFACE, true)
-OP(mouse_movement,        "Allow mouse clicks to move the player",
+OP(mouse_movement,        N_("Allow mouse clicks to move the player"),
 INTERFACE, true)
-OP(notify_recharge,       "Notify on object recharge",
+OP(notify_recharge,       N_("Notify on object recharge"),
 INTERFACE, false)
-OP(effective_speed,       "Show effective speed as multiplier",
+OP(effective_speed,       N_("Show effective speed as multiplier"),
 INTERFACE, false)
-OP(cheat_hear,            "Cheat: Peek into monster creation",
+OP(cheat_hear,            N_("Cheat: Peek into monster creation"),
 CHEAT, false)
-OP(score_hear,            "Score: Peek into monster creation",
+OP(score_hear,            N_("Score: Peek into monster creation"),
 SCORE, false)
-OP(cheat_room,            "Cheat: Peek into dungeon creation",
+OP(cheat_room,            N_("Cheat: Peek into dungeon creation"),
 CHEAT, false)
-OP(score_room,            "Score: Peek into dungeon creation",
+OP(score_room,            N_("Score: Peek into dungeon creation"),
 SCORE, false)
-OP(cheat_xtra,            "Cheat: Peek into something else",
+OP(cheat_xtra,            N_("Cheat: Peek into something else"),
 CHEAT, false)
-OP(score_xtra,            "Score: Peek into something else",
+OP(score_xtra,            N_("Score: Peek into something else"),
 SCORE, false)
-OP(cheat_live,            "Cheat: Allow player to avoid death",
+OP(cheat_live,            N_("Cheat: Allow player to avoid death"),
 CHEAT, false)
-OP(score_live,            "Score: Allow player to avoid death",
+OP(score_live,            N_("Score: Allow player to avoid death"),
 SCORE, false)
-OP(birth_randarts,        "Generate a new, random artifact set",
+OP(birth_randarts,        N_("Generate a new, random artifact set"),
 BIRTH, false)
-OP(birth_connect_stairs,  "Generate connected stairs",
+OP(birth_connect_stairs,  N_("Generate connected stairs"),
 BIRTH, true)
-OP(birth_force_descend,   "Force player descent (never make up stairs)",
+OP(birth_force_descend,   N_("Force player descent (never make up stairs)"),
 BIRTH, false)
-OP(birth_no_recall,       "Word of Recall has no effect",
+OP(birth_no_recall,       N_("Word of Recall has no effect"),
 BIRTH, false)
-OP(birth_no_artifacts,    "Restrict creation of artifacts",
+OP(birth_no_artifacts,    N_("Restrict creation of artifacts"),
 BIRTH, false)
-OP(birth_stacking,        "Stack objects on the floor",
+OP(birth_stacking,        N_("Stack objects on the floor"),
 BIRTH, true)
-OP(birth_lose_arts,       "Lose artifacts when leaving level",
+OP(birth_lose_arts,       N_("Lose artifacts when leaving level"),
 BIRTH, false)
-OP(birth_feelings,        "Show level feelings",
+OP(birth_feelings,        N_("Show level feelings"),
 BIRTH, true)
-OP(birth_no_selling,      "Increase gold drops but disable selling",
+OP(birth_no_selling,      N_("Increase gold drops but disable selling"),
 BIRTH, true)
-OP(birth_start_kit,       "Start with a kit of useful gear",
+OP(birth_start_kit,       N_("Start with a kit of useful gear"),
 BIRTH, true)
-OP(birth_ai_learn,        "Monsters learn from their mistakes",
+OP(birth_ai_learn,        N_("Monsters learn from their mistakes"),
 BIRTH, true)
-OP(birth_know_runes,      "Know all runes on birth",
+OP(birth_know_runes,      N_("Know all runes on birth"),
 BIRTH, false)
-OP(birth_know_flavors,    "Know all flavors on birth",
+OP(birth_know_flavors,    N_("Know all flavors on birth"),
 BIRTH, false)
-OP(birth_levels_persist,  "Persistent levels (experimental)",
+OP(birth_levels_persist,  N_("Persistent levels (experimental)"),
 BIRTH, false)
-OP(birth_percent_damage,  "To-damage is a percentage of dice (experimental)",
+OP(birth_percent_damage,  N_("To-damage is a percentage of dice (experimental)"),
 BIRTH, false)
 
